6ab056a48ebefcb3632076ffbe2e5667fef6939b
Rysker <mateuszlapka2001@gmail.com>
2024-05-16 16:45:54 +0200

    Endpoint adjustments.

---
+++ src/main/java/com/example/easyrent/dto/response/SignMessageDto.java
@@ -0,0 +1,13 @@
+package com.example.easyrent.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class SignMessageDto
+{
+    private String message;
+}
