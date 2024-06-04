e5daf4229dafaca7841a0c16ef90f072198561af
Rysker <mateuszlapka2001@gmail.com>
2024-06-05 00:03:26 +0200

    Added ticket endpoint.

--- src/main/java/com/example/easyrent/dto/response/MessageDto.java
+++ src/main/java/com/example/easyrent/dto/response/MessageDto.java
@@ -1,10 +1,14 @@
 package com.example.easyrent.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageDto
 {
     private String message;
